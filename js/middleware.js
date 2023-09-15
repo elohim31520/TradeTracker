1ded138bda20bb82070dc6c77668a128278013f0
lewis.lee <versus7063@gmail.com>
2023-09-15 14:45:20 +0800

    Error Handling for Expired or Missing JWT

--- js/middleware.js
+++ js/middleware.js
@@ -6,12 +6,12 @@ const publicKey = fs.readFileSync(path.join(__dirname, '../public.key'), 'utf8')
 function verifyToken(req, res, next) {
     const auth = req.headers.authorization
     const token = auth && auth.split(" ")[1]
-    if (!token) return res.status(401).json({ code: -1, message: "no Token provided" })
+    if (!token) throw new Error(401)
 
     jwt.verify(token, publicKey, (err, decoded) => {
         if (err) {
             console.log(err);
-            return res.status(401).json({ code: -2, message: "Token not Verified!" })
+            throw new Error(401)
         }
         next()
     })
