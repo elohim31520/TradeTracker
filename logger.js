3e40dc375d4b5df93f3ba5d968e0e08c86cc1346
lewis.lee <versus7063@gmail.com>
2023-09-26 18:38:22 +0800

    not in production then also log to the console with the colorized simple format.

--- logger.js
+++ logger.js
@@ -44,4 +44,13 @@ const logger = winston.createLogger({
 	],
 })
 
+if (process.env.NODE_ENV !== 'production') {
+	logger.add(new winston.transports.Console({
+		format: winston.format.combine(
+			winston.format.colorize(),
+			winston.format.simple()
+		)
+	}));
+}
+
 module.exports = logger
