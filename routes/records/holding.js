45aa2bb0c89365b71f78b891488c8386f1cd3d20
lewis.lee <versus7063@gmail.com>
2023-11-02 11:51:13 +0800

    Update the 'transactions' API to conform to RESTful style.

--- routes/records/holding.js
+++ routes/records/holding.js
@@ -3,17 +3,52 @@ const router = express.Router()
 const { verifyToken } = require("../../js/middleware")
 const modal = require("../../modal/records/holding")
 const { getRecordsBy, addRecords, delRecords, updateRecords } = require("./index")
-const { validateParamsOfGet } = require("./validate")
+const { successResponse } = require('../../js/config')
+const { validateParamsOfGet, validateParamsOfAdd, validateParamsOfDel } = require("./validate")
 
-router.get("/:userId", verifyToken, validateParamsOfGet, getRecordsBy(modal), (req, res) => {
-	res.json(req.records)
-})
+router.get("/:userId",
+	verifyToken,
+	validateParamsOfGet,
+	getRecordsBy(modal),
+	(req, res) => {
+		let resData = successResponse
+		resData.data = req.records
+		res.json(resData)
+	}
+)
 
-router.post("/add", verifyToken, addRecords(modal), (req, res) => {
-	res.json({code: 1, msg: "寫入成功"})
-})
+router.post("/",
+	verifyToken,
+	validateParamsOfAdd,
+	addRecords(modal),
+	(req, res) => {
+		res.json(successResponse)
+	}
+)
 
-router.post("/del", verifyToken, delRecords(modal))
-router.put("/put", verifyToken, updateRecords(modal))
+router.delete("/:id/:userId",
+	verifyToken,
+	validateParamsOfDel,
+	delRecords(modal),
+	(req, res) => {
+		res.json(successResponse)
+	}
+)
+
+router.patch("/:id",
+	verifyToken,
+	updateRecords(modal),
+	(req, res) => {
+		res.json(successResponse)
+	}
+)
+
+router.put("/:id",
+	verifyToken,
+	updateRecords(modal),
+	(req, res) => {
+		res.json(successResponse)
+	}
+)
 
 module.exports = router
