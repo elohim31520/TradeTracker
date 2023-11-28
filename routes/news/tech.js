c716426feb0155a52162cc4fdc2d174ba600d493
lewis.lee <versus7063@gmail.com>
2023-11-28 10:06:49 +0800

    Add API to Retrieve Tech News with Specific ID.

--- routes/news/tech.js
+++ routes/news/tech.js
@@ -19,6 +19,33 @@ const { successResponse } = require('../../js/config')
 // 	}
 // )
 
+router.get('/:id',
+	async (req, res) => {
+		let id = req.params.id
+		try {
+			const result = await TechNews.findOne({
+				where: {
+					id
+				},
+				attributes: [
+					'id', 'title', 'release_time', 'publisher', 'web_url',
+					[
+						sequelize.literal(`DATE_FORMAT(createdAt, '%Y-%m-%d')`),
+						'createdAt'
+					]
+				],
+			})
+
+			let resData = successResponse
+			resData.data = result
+			res.json(resData)
+		} catch (error) {
+			logger.error(e.message)
+			throw new Error(500)
+		}
+	}
+)
+
 router.post("/",
 	async (req, res) => {
 		let { pageIndex, pageSize, endDate, startDate, keyword } = req.body
@@ -36,7 +63,7 @@ router.post("/",
 					}
 				}
 			}
-			if(startDate && endDate) conditions.createdAt = {
+			if (startDate && endDate) conditions.createdAt = {
 				[Op.between]: [startDate, endDate],
 			}
 			const result = await TechNews.findAll({
