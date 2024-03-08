5db8b7d6089a3adb97ce9ea26737158dcc684593
lewis.lee <versus7063@gmail.com>
2024-03-08 14:18:40 +0800

    add migrations/20240308061300-modify_users_table

---
+++ migrations/20240308061300-modify_users_table.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		/**
+		 * Add altering commands here.
+		 *
+		 * Example:
+		 * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+		 */
+		return Promise.all([
+			queryInterface.addColumn('users', 'pwd', {
+				type: Sequelize.STRING,
+				allowNull: false,
+			}),
+			queryInterface.addColumn('users', 'salt', {
+				type: Sequelize.STRING,
+				allowNull: false,
+			}),
+		])
+	},
+
+	async down(queryInterface, Sequelize) {
+		/**
+		 * Add reverting commands here.
+		 *
+		 * Example:
+		 * await queryInterface.dropTable('users');
+		 */
+	}
+};
