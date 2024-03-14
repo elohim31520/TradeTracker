d446b569c9e38060d8bc1782eb86d9323327e218
lewis.lee <versus7063@gmail.com>
2024-03-14 12:32:53 +0800

    During API testing, write the token directly to token.txt upon login or registration.

--- test/user.js
+++ test/user.js
@@ -1,5 +1,8 @@
 const axios = require('axios')
 const { API_HOST } = require('./config')
+const fs = require('fs')
+const path = require('path')
+const { get } = require('lodash')
 
 function register({ user_name, pwd, email }) {
 	axios
@@ -10,6 +13,8 @@ function register({ user_name, pwd, email }) {
 		})
 		.then((res) => {
 			console.log(res.data)
+			const data = get(res, 'data.token', '')
+			writeToken(data)
 		})
 		.catch((err) => {
 			console.log(err)
@@ -24,10 +29,23 @@ function login({ user_name, pwd }) {
 		})
 		.then((res) => {
 			console.log(res.data)
+			const data = get(res, 'data.token', '')
+			writeToken(data)
 		})
 		.catch((err) => {
 			console.log(err)
 		})
 }
 
-register({ user_name: 'lewis7063', pwd: 'a123456+', email: 'lewis.lee@gmail.com' })
+function writeToken(data) {
+	fs.writeFile(path.join(__dirname, 'token.txt'), data, (err) => {
+		if (err) {
+			console.error('Error writing token to file:', err)
+		} else {
+			console.log('Token saved to token.txt')
+		}
+	})
+}
+
+// register({ user_name: 'lewis7063', pwd: 'a123456+', email: 'lewis.lee@gmail.com' })
+login({ user_name: 'lewis7063', pwd: 'a123456+' })
