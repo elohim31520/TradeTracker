74c6cc986d2f73b63cc8827b90c1193ac7798bf9
lewis.lee <versus7063@gmail.com>
2024-07-15 11:37:22 +0800

    feat: Remove unused files in routes/records
    This commit removes the following unused files from the  directory:
    
    * routes/records/index.js
    * routes/records/show.js
    * routes/records/edit.js
    * routes/records/update.js
    * routes/records/destroy.js

--- routes/news/tech.js
+++ routes/news/tech.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { verifyToken } = require('../../js/middleware');
+const { verifyToken } = require('../../middleware/auth');
 
 const TechNews = require("../../models/techNews")
 const sequelize = require("../../js/connect");
