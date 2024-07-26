29432bc675b9ec6cbe4209a335fd5add02285504
lewis.lee <versus7063@gmail.com>
2024-07-26 11:07:45 +0800

    Remove dead code

--- scripts/day.js
+++
@@ -1,5 +0,0 @@
-const dayjs = require("dayjs")
-
-let time = dayjs("Aug-23-23 08:55AM".split(":")[0]).format("YYYY-MM-DD")
-console.log(time);
-console.log(typeof time)
--- scripts/encode.js
+++
@@ -1,5 +0,0 @@
-const crypto = require('crypto-js')
-
-const password = "user_password";
-const encryptedPassword = crypto.SHA256(password).toString();
-console.log(encryptedPassword);
