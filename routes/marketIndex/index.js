c763feb7bcda4ab8d85fe05676c2039ed6ecbfa9
lewis.lee <versus7063@gmail.com>
2024-07-30 12:03:32 +0800

    Update momentum API calculation to include DXY and USOIL data

--- routes/marketIndex/index.js
+++ routes/marketIndex/index.js
@@ -4,24 +4,6 @@ const crud = require('../../crud/market_index')
 const { verifyToken } = require('../../middleware/auth')
 const { get } = require('lodash')
 
-function computeMomentum(btc, dxy) {
-	const w1 = 0.8
-	const w2 = 0.2
-	let sqBtc = btc ** 2
-	let sqDxy = dxy ** 2
-
-	if (btc < 0) sqBtc = -sqBtc
-	if (dxy < 0) sqDxy = -sqDxy
-
-	let weightedBtc = sqBtc * w1
-	let weightedDxy = sqDxy * w2
-
-	let scaledValue = weightedBtc - weightedDxy
-
-	return scaledValue
-}
-
-// 计算均值和标准差的函数
 function calculateMean(values) {
 	const sum = values.reduce((acc, val) => acc + val, 0)
 	return sum / values.length
@@ -40,35 +22,23 @@ router.get('/', verifyToken, async (req, res) => {
 
 router.get('/momentum', verifyToken, async (req, res) => {
 	const data = await crud.findAndGroupByTime()
-	const groupedData = data.reduce((acc, obj) => {
-		const existingGroup = acc.find((group) => group[0].createdAt === obj.createdAt)
-
-		if (existingGroup) {
-			existingGroup.push(obj)
-		} else {
-			acc.push([obj])
-		}
-		return acc
-	}, [])
 
 	const btcData = []
 	const usoilData = []
 	const dxyData = []
 
-	groupedData.forEach((subArray) => {
-		subArray.forEach((item) => {
-			switch (item.symbol) {
-				case 'BTCUSD':
-					btcData.push(item.price)
-					break
-				case 'USOIL':
-					usoilData.push(item.price)
-					break
-				case 'DXY':
-					dxyData.push(item.price)
-					break
-			}
-		})
+	data.forEach((item) => {
+		switch (item.symbol) {
+			case 'BTCUSD':
+				btcData.push(item)
+				break
+			case 'USOIL':
+				usoilData.push(item)
+				break
+			case 'DXY':
+				dxyData.push(item)
+				break
+		}
 	})
 
 	const getStandardizedValue = (values, currentValue) => {
@@ -80,11 +50,56 @@ router.get('/momentum', verifyToken, async (req, res) => {
 		return parseFloat(standardizedValue.toFixed(2))
 	}
 
-	const btc_standardized = btcData.map(price => getStandardizedValue(btcData, price))
-	// const usoil_standardized = usoilData.map(price => getStandardizedValue(usoilData, price))
-	// const dxy_standardized= dxyData.map(price => getStandardizedValue(dxyData, price))
+	const btc_prices = btcData.map((vo) => vo.price)
+	const btc_standardized = btcData.map((btcItem) => {
+		const volume = getStandardizedValue(btc_prices, btcItem.price)
+		return { ...btcItem, volume }
+	})
 
-	res.json(btc_standardized)
+	const dxy_prices = dxyData.map((vo) => vo.price)
+	const dxy_standardized = dxyData.map((dxyItem) => {
+		const volume = getStandardizedValue(dxy_prices, dxyItem.price)
+		return { ...dxyItem, volume }
+	})
+
+	const usoil_prices = usoilData.map((vo) => vo.price)
+	const usoil_standardized = usoilData.map((usoilItem) => {
+		const volume = getStandardizedValue(usoil_prices, usoilItem.price)
+		return { ...usoilItem, volume }
+	})
+
+	const consolidatedData = new Map()
+
+	const addDataToMap = (data) => {
+		data.forEach((item) => {
+			const existingItem = consolidatedData.get(item.createdAt) || {
+				createdAt: item.createdAt,
+				btc: 0,
+				usoil: 0,
+				dxy: 0,
+			}
+			if (item.symbol === 'BTCUSD') {
+				existingItem.btc = item.volume
+			} else if (item.symbol === 'USOIL') {
+				existingItem.usoil = item.volume
+			} else if (item.symbol === 'DXY') {
+				existingItem.dxy = item.volume
+			}
+			consolidatedData.set(item.createdAt, existingItem)
+		})
+	}
+
+	addDataToMap(btc_standardized)
+	addDataToMap(usoil_standardized)
+	addDataToMap(dxy_standardized)
+
+	const finalResult = Array.from(consolidatedData.values()).map((item) => {
+		const { createdAt, btc, usoil, dxy } = item
+		const volume = btc * 0.8 - 0.1 * dxy - 0.1 * usoil
+		return { createdAt, volume: parseFloat(volume.toFixed(2)) }
+	})
+
+	res.json(finalResult)
 })
 
 module.exports = router
