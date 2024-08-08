cc3a070664f5275db8ec711fa5402476cba1942b
lewis.lee <versus7063@gmail.com>
2024-08-08 18:06:15 +0800

    sort by value for pie chart visualization

--- schemas/transactionSchema.js
+++ schemas/transactionSchema.js
@@ -4,9 +4,10 @@ const createSchema = Joi.object({
 	stock_id: Joi.string().required(),
 	transaction_type: Joi.string().valid('buy', 'sell').required(),
 	quantity: Joi.number().integer().positive().required(),
-	price: Joi.number().precision(2).positive().required()
+	price: Joi.number().precision(2).positive().required(),
+	transaction_date: Joi.date().iso().required(),
 })
 
 module.exports = {
-	createSchema
+	createSchema,
 }
