16153feaae92e8358844115caef05266c4dc0ae1
lewis.lee <versus7063@gmail.com>
2025-02-12 11:42:46 +0800

    refactor: Optimize market index calculation algorithm

--- src/services/marketIndexService.js
+++ src/services/marketIndexService.js
@@ -6,18 +6,13 @@ const MOVING_AVERAGE = 10
 const BTCUSD = 'BTCUSD'
 const USOIL = 'USOIL'
 const DXY = 'DXY'
+const US10Y = 'US10Y'
 
 const KEY_MAP = {
 	BTCUSD: 'btc',
 	USOIL: 'usoil',
-	DXY: 'dxy'
-}
-
-const getStandardizedValue = (values, currentValue) => {
-	const mean = calculateMean(values)
-	const stdDev = calculateStdDev(values, mean)
-	const standardizedValue = (currentValue - mean) / stdDev
-	return parseFloat(standardizedValue.toFixed(2))
+	DXY: 'dxy',
+	US10Y: 'us10y',
 }
 
 class MarketIndexService {
@@ -66,58 +61,51 @@ class MarketIndexService {
 	async getMomentumData() {
 		const data = await this.getGroupedDataByTime()
 
-		const btcData = []
-		const usoilData = []
-		const dxyData = []
-
-		data.forEach((item) => {
-			switch (item.symbol) {
-				case BTCUSD:
-					btcData.push(item)
-					break
-				case USOIL:
-					usoilData.push(item)
-					break
-				case DXY:
-					dxyData.push(item)
-					break
-			}
-		})
-
-		const getMAPrices = (arr = [], movingAverage, currentIndex) => {
-			return arr.slice(Math.max(0, currentIndex - movingAverage), currentIndex)
-		}
-
-		const standardizeData = (data, movingAverage) => {
-			const prices = data.map((item) => item.price)
-			return data.map((item, index) => {
-				const windowPrices = getMAPrices(prices, movingAverage, index)
-				return { ...item, volume: getStandardizedValue(windowPrices, item.price) }
-			})
-		}
-
-		const btc_standardized = standardizeData(btcData, MOVING_AVERAGE)
-		const dxy_standardized = standardizeData(dxyData, MOVING_AVERAGE)
-		const usoil_standardized = standardizeData(usoilData, MOVING_AVERAGE)
-
 		const consolidatedData = new Map()
-
 		const addDataToMap = (data, key) => {
 			data.forEach(({ createdAt, volume }) => {
 				if (!consolidatedData.has(createdAt)) {
-					consolidatedData.set(createdAt, { createdAt, btc: 0, usoil: 0, dxy: 0 })
+					consolidatedData.set(createdAt, { createdAt, btc: 0, usoil: 0, dxy: 0, us10y: 0 })
 				}
 				consolidatedData.get(createdAt)[key] = volume
 			})
 		}
 
+		const getStandardizedValue = (values, currentValue) => {
+			const mean = calculateMean(values)
+			const stdDev = calculateStdDev(values, mean)
+			const diff = currentValue - mean
+			if (diff == 0) return 0
+			const standardizedValue = diff / stdDev
+			return parseFloat(standardizedValue.toFixed(2))
+		}
+
+		const standardizeData = (data) => {
+			const prices = data.map((item) => item.price)
+			return data.map((item, index) => {
+				const windowPrices = index < MOVING_AVERAGE ? prices : prices.slice(Math.max(0, index - MOVING_AVERAGE), index)
+				return { ...item, volume: getStandardizedValue(windowPrices, item.price) }
+			})
+		}
+
+		const btcData = data.filter((vo) => vo.symbol == BTCUSD)
+		const usoilData = data.filter((vo) => vo.symbol == USOIL)
+		const dxyData = data.filter((vo) => vo.symbol == DXY)
+		const us10YData = data.filter((vo) => vo.symbol == US10Y)
+
+		const btc_standardized = standardizeData(btcData)
+		const dxy_standardized = standardizeData(dxyData)
+		const usoil_standardized = standardizeData(usoilData)
+		const us10y_standardized = standardizeData(us10YData)
+
 		addDataToMap(btc_standardized, KEY_MAP[BTCUSD])
 		addDataToMap(usoil_standardized, KEY_MAP[USOIL])
 		addDataToMap(dxy_standardized, KEY_MAP[DXY])
+		addDataToMap(us10y_standardized, KEY_MAP[US10Y])
 
-		const finalResult = Array.from(consolidatedData.values()).map(({ createdAt, btc, usoil, dxy }) => ({
+		const finalResult = Array.from(consolidatedData.values()).map(({ createdAt, btc, usoil, dxy, us10y }) => ({
 			createdAt,
-			volume: parseFloat((btc - 0.1 * dxy - 0.1 * usoil).toFixed(2)),
+			volume: parseFloat((btc * 0.8 - 0.05 * dxy - 0.05 * usoil - 0.1 * us10y).toFixed(2)),
 		}))
 
 		return finalResult
