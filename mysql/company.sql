6b5479429dc05d9bc8e6397d45e0c5464fbbb5da
lewis.lee <versus7063@gmail.com>
2025-04-30 14:14:13 +0800

    Remove: Delete unused files

--- mysql/company.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Company(
-    symbol VARCHAR(4) NOT NULL UNIQUE,
-    name VARCHAR(32),
-    PRIMARY KEY (symbol)
-);
--- mysql/records/operate.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO records (id ,userId ,share ,price ,porfit_percent ,total ,company ,open_time)
-VALUES (1 ,"70eq6c1wvsf" ,5 ,6.6 ,7.7 ,"2%" ,50 ,"XOM" ,"2022-11-10");
-
-Select * from records group by company;
-
-select SUM(total) as total_account from records where userId = "lewis.lee";
