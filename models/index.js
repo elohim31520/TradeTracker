e9f4c0012c20b063a329b16f624e2d5fdd697c75
lewis.lee <versus7063@gmail.com>
2025-05-05 18:07:03 +0800

    refactor: Externalize test database credentials to .env for security

--- models/index.js
+++ models/index.js
@@ -1,52 +1,51 @@
-'use strict';
+'use strict'
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+require('dotenv').config()
+const fs = require('fs')
+const path = require('path')
+const Sequelize = require('sequelize')
+const process = require('process')
+const basename = path.basename(__filename)
+const env = process.env.NODE_ENV || 'development'
+const config = require(__dirname + '/../config/config.json')[env]
+const db = {}
 
-let sequelize;
+let sequelize
 if (config.use_env_variable) {
+	// prd
 	sequelize = new Sequelize(process.env[config.use_env_variable], {
-        ...config,
-        timezone: '+08:00' // 添加時區設定
-    });
+		...config,
+		timezone: '+08:00',
+	})
 } else {
-	sequelize = new Sequelize(config.database, config.username, config.password, {
-        ...config,
-        timezone: '+08:00' // 添加時區設定
-    });
+	sequelize = new Sequelize(process.env['DB_NAME'], process.env['DB_USER'], process.env['DB_PASSWORD'], {
+		...config,
+		timezone: '+08:00',
+	})
 }
 
-fs
-	.readdirSync(__dirname)
-	.filter(file => {
+fs.readdirSync(__dirname)
+	.filter((file) => {
 		return (
-			![
-				'index.js',
-			].includes(file) &&
+			!['index.js'].includes(file) &&
 			file.indexOf('.') !== 0 &&
 			file !== basename &&
 			file.slice(-3) === '.js' &&
 			file.indexOf('.test.js') === -1
-		);
+		)
 	})
-	.forEach(file => {
-		const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-		db[model.name] = model;
-	});
+	.forEach((file) => {
+		const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
+		db[model.name] = model
+	})
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
 	if (db[modelName].associate) {
-		db[modelName].associate(db);
+		db[modelName].associate(db)
 	}
-});
+})
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelize
+db.Sequelize = Sequelize
 
-module.exports = db;
+module.exports = db
