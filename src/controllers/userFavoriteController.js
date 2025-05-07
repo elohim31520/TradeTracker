45532a9c184698c4bf2671d96bb2c09009015271
lewis.lee <versus7063@gmail.com>
2025-05-07 12:00:40 +0800

    feat: Add getUserNameFromReq utility function

--- src/controllers/userFavoriteController.js
+++ src/controllers/userFavoriteController.js
@@ -2,13 +2,14 @@ const _get = require('lodash/get')
 const logger = require('../logger.js')
 const userFavoriteService = require('../services/userFavoriteService')
 const responseHelper = require('../js/responseHelper')
+const { getUserNameFromReq } = require('../js/util')
 
 /**
  * 創建用戶收藏技術新聞
  */
 const createUserFavoriteTechNews = async (req, res) => {
 	try {
-		const userName = _get(req.decoded, 'user_name', '')
+		const userName = getUserNameFromReq(req)
 		const newsId = _get(req, 'body.newsId')
 		await userFavoriteService.createUserFavoriteTechNews(userName, newsId)
 		res.json(responseHelper.success())
@@ -22,7 +23,7 @@ const createUserFavoriteTechNews = async (req, res) => {
  * 獲取用戶收藏技術新聞
  */
 const getUserFavoriteTechNews = async (req, res) => {
-	const userName = _get(req.decoded, 'user_name', '')
+	const userName = getUserNameFromReq(req)
 	try {
 		const data = await userFavoriteService.getUserFavoriteTechNews(userName)
 		res.json(responseHelper.success(data))
