ec7bb967f5cbdda9a7dbd296e4e1ecdc4b4411ef
lewis.lee <versus7063@gmail.com>
2025-05-12 16:15:33 +0800

    Refactor: Update error handling to return a unified format

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -16,7 +16,6 @@ class UserController {
 			const result = await userService.login(req.body)
 			res.status(200).json(responseHelper.success(result))
 		} catch (error) {
-			console.log(error)
 			next(error)
 		}
 	}
--- src/controllers/userFavoriteController.js
+++ src/controllers/userFavoriteController.js
@@ -7,29 +7,27 @@ const { getUserNameFromReq } = require('../js/util')
 /**
  * 創建用戶收藏技術新聞
  */
-const createUserFavoriteTechNews = async (req, res) => {
+const createUserFavoriteTechNews = async (req, res, next) => {
 	try {
 		const userName = getUserNameFromReq(req)
 		const newsId = _get(req, 'body.newsId')
 		await userFavoriteService.createUserFavoriteTechNews(userName, newsId)
 		res.json(responseHelper.success())
-	} catch (e) {
-		logger.error(e.message)
-		throw new Error(500)
+	} catch (error) {
+		next(error)
 	}
 }
 
 /**
  * 獲取用戶收藏技術新聞
  */
-const getUserFavoriteTechNews = async (req, res) => {
+const getUserFavoriteTechNews = async (req, res, next) => {
 	const userName = getUserNameFromReq(req)
 	try {
 		const data = await userFavoriteService.getUserFavoriteTechNews(userName)
 		res.json(responseHelper.success(data))
-	} catch (e) {
-		logger.error(e.message)
-		throw new Error(500)
+	} catch (error) {
+		next(error)
 	}
 }
 
