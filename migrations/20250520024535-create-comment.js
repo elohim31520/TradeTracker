2dbb669309751d50dbc2ca3e557fae0b4d0ccbfd
lewis.lee <versus7063@gmail.com>
2025-05-20 11:09:36 +0800

    feat: Add Comment table to store user comments

---
+++ migrations/20250520024535-create-comment.js
@@ -0,0 +1,66 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable('Comments', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			content: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				comment: '評論內容',
+			},
+			postId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: {
+					model: 'company_news', // Changed from 'CompanyNews' to 'company_news'
+					key: 'id',
+				},
+				comment: '新聞id',
+			},
+			userId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: {
+					model: 'Users',
+					key: 'id',
+				},
+				comment: '評論用戶id',
+			},
+			toUserId: {
+				type: Sequelize.INTEGER,
+				allowNull: true,
+				references: {
+					model: 'Users',
+					key: 'id',
+				},
+				comment: '被評論用戶id',
+			},
+			parentId: {
+				type: Sequelize.INTEGER,
+				allowNull: true,
+				references: {
+					model: 'Comments',
+					key: 'id',
+				},
+				comment: '父評論id',
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		})
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('Comments')
+	},
+}
---
+++ models/comment.js
@@ -0,0 +1,76 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+	class Comment extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			Comment.belongsTo(models.Users, {
+				foreignKey: 'userId',
+				as: 'author',
+			})
+			Comment.belongsTo(models.Users, {
+				foreignKey: 'toUserId',
+				as: 'toUser',
+			})
+			Comment.belongsTo(models.Comment, {
+				foreignKey: 'parentId',
+				as: 'parent',
+			})
+			Comment.belongsTo(models.CompanyNews, {
+				foreignKey: 'postId',
+				as: 'newsPost',
+			})
+		}
+	}
+	Comment.init(
+		{
+			content: DataTypes.STRING,
+			postId: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				references: {
+					model: 'CompanyNews',
+					key: 'id',
+				},
+				comment: '新聞id',
+			},
+			userId: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				references: {
+					model: 'Users',
+					key: 'id',
+				},
+				comment: '評論用戶id',
+			},
+			toUserId: {
+				type: DataTypes.INTEGER,
+				allowNull: true,
+				references: {
+					model: 'Users',
+					key: 'id',
+				},
+				comment: '被評論用戶id',
+			},
+			parentId: {
+				type: DataTypes.INTEGER,
+				allowNull: true,
+				comment: '父評論id',
+				references: {
+					model: 'Comments',
+					key: 'id',
+				},
+			},
+		},
+		{
+			sequelize,
+			modelName: 'Comments',
+		}
+	)
+	return Comment
+}
