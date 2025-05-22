897df3b987e69978c663c72c49a8e5b65b2edf69
lewis.lee <versus7063@gmail.com>
2025-05-22 11:14:47 +0800

    Refactor: Simplify comment controller error handling with next()

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -1,7 +1,6 @@
 'use strict'
-const commentService = require('../service/commentService')
+const commentService = require('../services/commentService')
 const { success, fail } = require('../js/responseHelper')
-const errorCodes = require('../constant/errorCodes')
 const { getUserNameFromReq } = require('../js/util')
 
 class CommentController {
@@ -10,15 +9,14 @@ class CommentController {
 	 * @param {Object} req - 請求對象
 	 * @param {Object} res - 響應對象
 	 */
-	async createComment(req, res) {
+	async createComment(req, res, next) {
 		try {
 			const data = req.body
 			const userName = getUserNameFromReq(req)
 			const comments = await commentService.createComment({ ...data, userName })
 			return res.status(201).json(success(comments, '評論創建成功'))
 		} catch (error) {
-			const SERVER_ERROR = errorCodes.SERVER_ERROR.code
-			return res.status(SERVER_ERROR).json(fail(SERVER_ERROR, error.message))
+			next(error)
 		}
 	}
 
@@ -27,15 +25,15 @@ class CommentController {
 	 * @param {Object} req - 請求對象
 	 * @param {Object} res - 響應對象
 	 */
-	async getCommentsByPostId(req, res) {
+	async getCommentsByPostId(req, res, next) {
 		try {
 			const { postId } = req.params
 			const options = req.query
 			const comments = await commentService.getCommentsByPostId(postId, options)
 			return res.json(success(comments))
 		} catch (error) {
-			const SERVER_ERROR = errorCodes.SERVER_ERROR.code
-			return res.status(SERVER_ERROR).json(fail(SERVER_ERROR, error.message))
+			const code = errorCodes.SERVER_ERROR.code
+			return res.status(code).json(fail(code, error.message))
 		}
 	}
 
@@ -44,7 +42,7 @@ class CommentController {
 	 * @param {Object} req - 請求對象
 	 * @param {Object} res - 響應對象
 	 */
-	async getRepliesByParentId(req, res) {
+	async getRepliesByParentId(req, res, next) {
 		try {
 			const { parentId } = req.params
 			const options = req.query
@@ -52,8 +50,7 @@ class CommentController {
 			const replies = await commentService.getRepliesByParentId(parentId, options)
 			return res.json(success(replies))
 		} catch (error) {
-			const SERVER_ERROR = errorCodes.SERVER_ERROR.code
-			return res.status(SERVER_ERROR).json(fail(SERVER_ERROR, error.message))
+			next(error)
 		}
 	}
 
@@ -62,14 +59,13 @@ class CommentController {
 	 * @param {Object} req - 請求對象
 	 * @param {Object} res - 響應對象
 	 */
-	async getCommentById(req, res) {
+	async getCommentById(req, res, next) {
 		try {
 			const { id } = req.params
 			const comments = await commentService.getCommentById(id)
 			return res.json(success(comments))
 		} catch (error) {
-			const SERVER_ERROR = errorCodes.SERVER_ERROR.code
-			return res.status(SERVER_ERROR).json(fail(SERVER_ERROR, error.message))
+			next(error)
 		}
 	}
 
@@ -78,15 +74,14 @@ class CommentController {
 	 * @param {Object} req - 請求對象
 	 * @param {Object} res - 響應對象
 	 */
-	async updateComment(req, res) {
+	async updateComment(req, res, next) {
 		try {
 			const { id } = req.params
 			const data = req.body
 			const updatedComment = await commentService.updateComment(id, data)
 			return res.json(success(updatedComment, '評論更新成功'))
 		} catch (error) {
-			const SERVER_ERROR = errorCodes.SERVER_ERROR.code
-			return res.status(SERVER_ERROR).json(fail(SERVER_ERROR, error.message))
+			next(error)
 		}
 	}
 
@@ -95,14 +90,13 @@ class CommentController {
 	 * @param {Object} req - 請求對象
 	 * @param {Object} res - 響應對象
 	 */
-	async deleteComment(req, res) {
+	async deleteComment(req, res, next) {
 		try {
 			const { id } = req.params
 			await commentService.deleteComment(id)
 			return res.json(success([], '評論刪除成功'))
 		} catch (error) {
-			const SERVER_ERROR = errorCodes.SERVER_ERROR.code
-			return res.status(SERVER_ERROR).json(fail(SERVER_ERROR, error.message))
+			next(error)
 		}
 	}
 }
