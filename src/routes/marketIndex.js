8a53dffb95de47c195b429b99f7254cefc68b074
lewis.lee <versus7063@gmail.com>
2025-06-06 10:50:02 +0800

    refactor: Implement Redis caching for /market APIs

--- src/routes/marketIndex.js
+++ src/routes/marketIndex.js
@@ -9,12 +9,17 @@ const redisCacheMiddleware = require('../middleware/redisCacheMiddleware')
 router.get('/', marketIndexController.getAll)
 router.get('/momentum', verifyToken, marketIndexController.getMomentum)
 router.get('/last/:symbol', validate(getLastOneSchema, 'params'), marketIndexController.getLstOne)
-router.get('/momentum/range/:days', verifyToken, validate(getByDaysSchema, 'params'), marketIndexController.getMarketIndicesByDays)
+router.get(
+	'/momentum/range/:days',
+	verifyToken,
+	validate(getByDaysSchema, 'params'),
+	marketIndexController.getMarketIndicesByDays
+)
 router.get('/weights', verifyToken, redisCacheMiddleware(3600), marketIndexController.getWeights)
-router.get('/stock/prices', verifyToken, marketIndexController.getStockPrices)
-router.get('/stock/symbols', verifyToken, marketIndexController.getStockSymbol)
-router.get('/stock/breadth', verifyToken, marketIndexController.getMarketBreadth)
-router.get('/stock/winners', verifyToken, marketIndexController.getStockWinners)
-router.get('/stock/losers', verifyToken, marketIndexController.getStockLosers)
+router.get('/stock/prices', verifyToken, redisCacheMiddleware(3600), marketIndexController.getStockPrices)
+router.get('/stock/symbols', verifyToken, redisCacheMiddleware(3600), marketIndexController.getStockSymbol)
+router.get('/stock/breadth', verifyToken, redisCacheMiddleware(3600), marketIndexController.getMarketBreadth)
+router.get('/stock/winners', verifyToken, redisCacheMiddleware(3600), marketIndexController.getStockWinners)
+router.get('/stock/losers', verifyToken, redisCacheMiddleware(3600), marketIndexController.getStockLosers)
 
 module.exports = router
