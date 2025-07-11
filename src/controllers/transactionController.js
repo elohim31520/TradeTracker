6456f7ec69287adf0e90314fdd202a40cb6c4fd8
lewis.lee <versus7063@gmail.com>
2025-07-11 10:57:32 +0800

    remove unused code

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -48,27 +48,6 @@ class TransactionController {
 			next(error)
 		}
 	}
-
-	async clone(req, res, next) {
-		try {
-			const transactions = await transactionService.getAll()
-			const fs = require('fs')
-			const path = require('path')
-
-			const dbFolderPath = path.join(__dirname, '..', 'DB')
-			if (!fs.existsSync(dbFolderPath)) {
-				fs.mkdirSync(dbFolderPath)
-			}
-
-			const filePath = path.join(dbFolderPath, 'transactions.json')
-			const dataToWrite = JSON.stringify(transactions, null, 2)
-
-			await fs.promises.writeFile(filePath, dataToWrite, 'utf8')
-			res.json(responseHelper.success([], 'Transactions cloned successfully'))
-		} catch (error) {
-			next(error)
-		}
-	}
 }
 
 module.exports = new TransactionController()
