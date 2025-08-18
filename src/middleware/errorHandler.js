2312925d9e6c22d943cfb9e93d5864d3302af11a
lewis.lee <versus7063@gmail.com>
2025-08-18 11:53:40 +0800

    refactor: 優化errorHandler 代碼，並且加上logger 寫入到elasticsearch

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,26 +1,55 @@
 const { AppError, ClientError, AuthError } = require('../modules/errors')
 const responseHelper = require('../modules/responseHelper')
 const errorCodes = require('../constant/errorCodes').default
+const logger = require('../logger')
+
+const errorHandlers = [
+	{
+		matches: (err) => err instanceof AuthError,
+		handle: (err, res) => {
+			res.status(401).json(responseHelper.fail(errorCodes.UNAUTHORIZED.code, err.message))
+		},
+	},
+	{
+		matches: (err) => err instanceof ClientError,
+		handle: (err, res) => {
+			const statusCode = errorCodes.MISSING_REQUIRED_PARAM.code
+			res.status(statusCode).json(responseHelper.fail(statusCode, err.message))
+		},
+	},
+	{
+		matches: (err) => err instanceof AppError,
+		handle: (err, res) => {
+			res.status(err.statusCode).json(responseHelper.fail(err.statusCode, err.message))
+		},
+	},
+	{
+		matches: (err) => err.name === 'SequelizeUniqueConstraintError',
+		handle: (res) => {
+			const { code, message } = errorCodes.DUPLICATE_ACCOUNT
+			res.status(409).json(responseHelper.fail(code, message))
+		},
+	},
+	{
+		matches: (err) => err.name === 'SequelizeDatabaseError',
+		handle: (res) => {
+			const { code, message } = errorCodes.WRONG_VALUE_FOR_FIELD
+			res.status(400).json(responseHelper.fail(code, message))
+		},
+	},
+]
 
 const errHandler = (err, req, res, next) => {
-	err.statusCode = err.statusCode
-	err.status = err.status
-	if (err instanceof AuthError) {
-		res.json(responseHelper.fail(errorCodes.UNAUTHORIZED.code, err.message))
-	} else if (err instanceof ClientError) {
-		res
-			.status(errorCodes.MISSING_REQUIRED_PARAM.code)
-			.json(responseHelper.fail(errorCodes.MISSING_REQUIRED_PARAM.code, err.message))
-	} else if (err instanceof AppError) {
-		res.status(err.statusCode).json(responseHelper.fail(err.statusCode, err.message))
-	} else if (err.name == 'SequelizeUniqueConstraintError') {
-		res.json(responseHelper.fail(errorCodes.DUPLICATE_ACCOUNT.code, errorCodes.DUPLICATE_ACCOUNT.message))
-	} else if (err.name == 'SequelizeDatabaseError') {
-		res.json(responseHelper.fail(errorCodes.WRONG_VALUE_FOR_FIELD.code, errorCodes.WRONG_VALUE_FOR_FIELD.message))
-	} else {
-		console.error('ERROR 💥', err)
-		res.json(responseHelper.fail(errorCodes.SERVER_ERROR.code, errorCodes.SERVER_ERROR.message))
+	for (const handler of errorHandlers) {
+		if (handler.matches(err)) {
+			return handler.handle(err, res)
+		}
 	}
+
+	console.error('ERROR 💥', err)
+	logger.error(err)
+	const { code, message } = errorCodes.SERVER_ERROR
+	res.status(500).json(responseHelper.fail(code, message))
 }
 
 module.exports = errHandler
