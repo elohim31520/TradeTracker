a6278c7c28715492061eaefd2d3f256eb0708490
lewis.lee <versus7063@gmail.com>
2025-08-22 11:42:50 +0800

    feat: 新增migration刪除無用的company-news資料表

---
+++ migrations/20250822034019-drop-company-news-table.js
@@ -0,0 +1,48 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.dropTable('company_news')
+	},
+
+	async down(queryInterface, Sequelize) {
+		await queryInterface.createTable('company_news', {
+			id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			title: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				unique: true,
+			},
+			company_id: {
+				type: Sequelize.INTEGER,
+				references: {
+					model: 'Company',
+					key: 'id',
+				},
+			},
+			release_time: {
+				type: Sequelize.DATE,
+			},
+			publisher: {
+				type: Sequelize.STRING,
+			},
+			web_url: {
+				type: Sequelize.STRING,
+			},
+			createdAt: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			updatedAt: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+		})
+	},
+}
