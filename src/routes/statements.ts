3192b650aea4d28ca94191627198047c4bb45147
lewis.lee <versus7063@gmail.com>
2025-09-05 11:57:17 +0800

    feat: 股票相關指標資料用redis緩存

--- src/routes/statements.ts
+++ src/routes/statements.ts
@@ -2,6 +2,8 @@ import express, { Router } from 'express'
 import statementController from '../controllers/statementController'
 import validate from '../middleware/validate'
 import { getBySymbolSchema, getBySymbolWithDaysSchema } from '../schemas/statementSchema'
+const redisCache = require('../middleware/redisCache')
+const { DAILY_UPDATE_CACHE_TTL } = require('../constant/cache')
 
 const router: Router = express.Router()
 
@@ -9,6 +11,7 @@ router.get(
 	'/:symbol',
 	validate(getBySymbolSchema, 'params'),
 	validate(getBySymbolWithDaysSchema, 'query'),
+	redisCache(DAILY_UPDATE_CACHE_TTL),
 	statementController.getBySymbol
 )
 
