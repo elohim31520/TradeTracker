923e37f0483f9202de5b55134117975b7a323741
lewis.lee <versus7063@gmail.com>
2025-09-18 15:27:38 +0800

    fix: migration大小寫問題

--- migrations/20250610090334-change-date-to-string.js
+++ migrations/20250610090334-change-date-to-string.js
@@ -3,14 +3,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
 	async up(queryInterface, Sequelize) {
-		await queryInterface.changeColumn('StockPrices', 'date', {
+		await queryInterface.changeColumn('stock_prices', 'date', {
 			type: Sequelize.STRING(32),
 			allowNull: false,
 		})
 	},
 
 	async down(queryInterface, Sequelize) {
-		await queryInterface.changeColumn('StockPrices', 'date', {
+		await queryInterface.changeColumn('stock_prices', 'date', {
 			type: Sequelize.DATE,
 			allowNull: false,
 		})
