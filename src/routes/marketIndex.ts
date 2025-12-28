60cfc33144cbac010f6202be79a0b6d188a08522
lewis.lee <versus7063@gmail.com>
2025-12-28 22:47:37 +0800

    update: momentun開放3天的存取，無須登入驗證

--- src/routes/marketIndex.ts
+++ src/routes/marketIndex.ts
@@ -5,7 +5,7 @@ import validate from '../middleware/validate'
 import { getLastOneSchema, getByDaysSchema } from '../schemas/marketIndexSchema'
 import redisCache from '../middleware/redisCache'
 import conditionalCache from '../middleware/conditionalCache'
-import { DAILY_UPDATE_CACHE_TTL, HOURS3_CACHE_TTL } from '../constant/cache'
+import { DAILY_UPDATE_CACHE_TTL, HOUR_CACHE_TTL } from '../constant/cache'
 
 const router = express.Router()
 
@@ -24,7 +24,7 @@ router.get('/momentum', verifyToken, marketController.getMomentum)
 router.get('/weights', verifyToken, redisCache(DAILY_UPDATE_CACHE_TTL), marketController.getWeights)
 router.get('/quotes', marketController.getQuotes  )
 
-// 當 days 為 1 時，不需驗證 token 並強制快取，這是前端的需求, 3小時快取
+// 當 days 為 1 時，不需驗證 token 並強制快取，這是前端的需求, 1小時快取
 router.get(
 	'/momentum/range/1',
 	// 手動設定 params.days 以便後續的 validate 和 controller 能正確取值
@@ -33,7 +33,18 @@ router.get(
 		return next();
 	},
 	validate(getByDaysSchema, 'params'),
-	redisCache(HOURS3_CACHE_TTL),
+	redisCache(HOUR_CACHE_TTL),
+	marketController.getMarketIndicesByDays
+)
+
+router.get(
+	'/momentum/range/3',
+	(req, res, next) => {
+		req.params.days = '3';
+		return next();
+	},
+	validate(getByDaysSchema, 'params'),
+	redisCache(DAILY_UPDATE_CACHE_TTL), //快取1天
 	marketController.getMarketIndicesByDays
 )
 
